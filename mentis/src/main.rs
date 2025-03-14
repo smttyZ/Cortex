683b92c6a8b14cf0704befaabe8c375481b75354
oracle-windows <smttyz4@gmail.com>
2025-03-14 14:16:19 -0400

    Rust: Updated for linux branch

--- mentis/src/main.rs
+++ mentis/src/main.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3001").expect("Failed to bind port!");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
