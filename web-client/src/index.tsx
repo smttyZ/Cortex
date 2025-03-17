0540ff7fa1a06bb2697b87981da343974659bf39
oracle-windows <smttyz4@gmail.com>
2025-03-17 16:25:16 -0400

    Web: Small personalizaton

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-    return <h1>Hello from React + TSX!</h1>;
+    return <h1>Hi Mom!</h1>;
 };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
