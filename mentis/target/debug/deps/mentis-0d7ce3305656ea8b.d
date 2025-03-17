6b38d5440b6411c1e8c85b5dd463332d3c80043c
oracle-windows <smttyz4@gmail.com>
2025-03-17 17:36:11 -0400

    git: Remove mentis/target dir from repo

--- mentis/target/debug/deps/mentis-0d7ce3305656ea8b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\smtty\Documents\GitHub\Cortex\mentis\target\debug\deps\mentis-0d7ce3305656ea8b.exe: src\main.rs
-
-C:\Users\smtty\Documents\GitHub\Cortex\mentis\target\debug\deps\mentis-0d7ce3305656ea8b.d: src\main.rs
-
-src\main.rs:
